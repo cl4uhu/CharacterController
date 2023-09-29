155a6bff6f34c1702f743042530a7d5f58add991
cl4uhu <pjclaudia1@gmail.com>
2023-09-29 10:49:05 +0200

    character jump

--- Assets/Scripts/IsometricController.cs
+++ Assets/Scripts/IsometricController.cs
@@ -8,9 +8,21 @@ public class IsometricController : MonoBehaviour
     private float _horizontal;
     private float _vertical;
 
+    //variables para velocidad, salto y gravedad
     [SerializeField] private float playerSpeed = 5;
+    [SerializeField] private float _jumpHeight = 1; 
+    private float _gravity = -9.81f; 
+    private Vector3 _playerGravity; 
+    
+    //variables para rotacion
     private float turnSmoothVelocity;
     [SerializeField] float turnSmoothTime = 0.1f; 
+
+    //variable para sensor
+    [SerializeField] private Transform _sensorPosition;
+    [SerializeField] private float _sensorRadius = 0.3f;
+    [SerializeField] private LayerMask _groundLayer;
+    private bool _isGrounded;
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +37,7 @@ public class IsometricController : MonoBehaviour
         _vertical = Input.GetAxisRaw("Vertical");
 
         Movement();
+        Jump();
     }
     void Movement()
     {
@@ -37,8 +50,27 @@ public class IsometricController : MonoBehaviour
 
             transform.rotation = Quaternion.Euler(0, smoothAngle, 0);
 
-            _controller.Move(direction * playerSpeed * Time.deltaTime); 
+            _controller.Move(direction.normalized * playerSpeed * Time.deltaTime); 
         } 
+    }
+    
+    void Jump()
+    {
+        _isGrounded = Physics.CheckSphere(_sensorPosition.position, _sensorRadius, _groundLayer); 
 
+        if(_isGrounded && _playerGravity.y < 0)
+        {
+            _playerGravity.y = 0;
+        }
+        
+        if(_isGrounded && Input.GetButtonDown("Jump"))
+        {
+            //_playerGravity.y = -_jumpHeight; 
+            _playerGravity.y = Mathf.Sqrt(_jumpHeight * -2 * _gravity); 
+        }
+
+        _playerGravity.y += _gravity * Time.deltaTime;
+
+        _controller.Move(_playerGravity * Time.deltaTime);
     }
 }
