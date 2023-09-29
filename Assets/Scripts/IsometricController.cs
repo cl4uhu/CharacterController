ba2517cc54ee66c8f22fb9287db247afce57c6c8
cl4uhu <pjclaudia1@gmail.com>
2023-09-29 10:00:54 +0200

    smooth rotation

--- Assets/Scripts/IsometricController.cs
+++ Assets/Scripts/IsometricController.cs
@@ -9,6 +9,8 @@ public class IsometricController : MonoBehaviour
     private float _vertical;
 
     [SerializeField] private float playerSpeed = 5;
+    private float turnSmoothVelocity;
+    [SerializeField] float turnSmoothTime = 0.1f; 
 
     // Start is called before the first frame update
     void Start()
@@ -30,9 +32,10 @@ public class IsometricController : MonoBehaviour
 
         if(direction != Vector3.zero)
         {
-           float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+           float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg; //movimiento
+           float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime); //movimiento suavizado, rotación actual a la que quiero llegar indicando su tiempo 
 
-            transform.rotation = Quaternion.Euler(0, targetAngle, 0);
+            transform.rotation = Quaternion.Euler(0, smoothAngle, 0);
 
             _controller.Move(direction * playerSpeed * Time.deltaTime); 
         } 
