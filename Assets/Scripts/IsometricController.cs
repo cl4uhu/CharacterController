0fcff2a559cbb34957fa2e5d6702d45e1caa49e3
cl4uhu <pjclaudia1@gmail.com>
2023-09-29 09:28:29 +0200

    isometric controller script

--- Assets/Scripts/IsometricController.cs
+++ Assets/Scripts/IsometricController.cs
@@ -2,8 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class IsometricController : MonoBehaviour
 {
+    private CharacterControloler _controller;
+    private float _horizxontal;
+    private float _vertical;
+
+    [SerializeField] private float playerSpeed = 5;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,15 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        _horizontal = Input.GetAxisRaw("Horizontal");
+        _vertical = Input.GetAxisRaw("Vertical");
+
+        Movement();
+    }
+    void Movement()
+    {
+        Vector3 direction = new Vector3(_horizontal, 0, _vertical);
+
+        _controller.Move(direction * playerSpeed * Time.deltaTime);
     }
 }
